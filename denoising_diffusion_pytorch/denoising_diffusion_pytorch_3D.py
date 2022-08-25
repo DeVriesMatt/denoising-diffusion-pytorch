update dataset to correct normalisation

denoising_diffusion_pytorch/denoising_diffusion_pytorch_3D.py
@@ -822,6 +822,7 @@ class SingleCell(Dataset):
         transforms=None,
         image_size=400,
         cell_component="both",
+        rescale_int=tio.Compose([tio.RescaleIntensity(out_min_max=(0, 1))]),
     ):
         self.image_path = image_path
         self.dataframe_path = dataframe_path
@@ -830,6 +831,7 @@ class SingleCell(Dataset):
         self.transforms = transforms
         self.image_size = image_size
         self.cell_component = cell_component
+        self.rescale_int = rescale_int
         choices = ["cell", "nuc", "both"]
         assert cell_component in choices, f"Please choose one of {choices}."
 
@@ -859,6 +861,7 @@ class SingleCell(Dataset):
                 self.image_path, plate_num, component_path, serial_number + ".tif"
             )
             image = io.imread(path)
+
         else:
             cell_component_path = "stacked_intensity_cell"
             try:
@@ -884,15 +887,24 @@ class SingleCell(Dataset):
                 self.image_path, plate_num, nuc_component_path, serial_number + ".tif"
             )
             nuc_image = io.imread(nuc_path)
+
+            cell_image = self.rescale_int(
+                torch.tensor(cell_image.astype(np.float16)).unsqueeze(0)
+            )
+            nuc_image = self.rescale_int(
+                torch.tensor(nuc_image.astype(np.float16)).unsqueeze(0)
+            )
             try:
-                image = np.stack((cell_image, nuc_image))
+                image = torch.stack(
+                    (torch.squeeze(cell_image), torch.squeeze(nuc_image))
+                )
             except:
                 image = torch.ones((2, 64, 64, 64))
 
         if self.transforms:
             image = self.transforms(image)
 
-        return torch.Tensor(image)
+        return image
 
 
 class Dataset(Dataset):
@@ -993,9 +1005,8 @@ class Trainer3D(object):
         transform = tio.Compose(
             [
                 tio.CropOrPad((self.image_size, self.image_size, self.image_size)),
-                tio.OneOf(spatial_transforms, p=0.5),
+                tio.OneOf(spatial_transforms, p=0.8),
                 tio.RandomFlip(axes=["LR", "AP", "IS"]),
-                tio.RescaleIntensity(out_min_max=(0, 1)),
             ]
         )
 