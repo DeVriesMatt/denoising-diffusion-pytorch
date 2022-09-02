1ab73c0bb9d467678819e55a32427d25d19b1c8e
DeVriesMatt <matthew.devries.13@gmail.com>
2022-09-02 16:27:35 +0100

    turn off amp

--- train_3D.py
+++ train_3D.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
         train_num_steps=args.num_steps,  # total training steps
         gradient_accumulate_every=2,  # gradient accumulation steps
         ema_decay=0.995,  # exponential moving average decay
-        amp=True,  # turn on mixed precision,
+        amp=False,  # turn on mixed precision,
         results_folder=args.output_dir + "/results/"
     )
 
